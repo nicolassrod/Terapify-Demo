02156ac94413d4fb252203b67d6448896475f7e6
Nicolás A. Rodríguez <nrq5510@gmail.com>
2020-07-14 11:52:57 -0500

    Initial Commit

---
+++ Terapify Demo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Terapify Demo
+//
+//  Created by Nicolás A. Rodríguez on 7/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Terapify Demo/Terapify_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Terapify_DemoApp.swift
+//  Terapify Demo
+//
+//  Created by Nicolás A. Rodríguez on 7/14/20.
+//
+
+import SwiftUI
+
+@main
+struct Terapify_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
